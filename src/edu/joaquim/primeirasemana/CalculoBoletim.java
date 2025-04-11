c84799461f533ab871f8387bb9d18ad9d6de0cec
Joaquim-Azevedo <joaquimazeveddoo@gmail.com>
2025-04-10 23:18:18 -0300

    Adicionando Entrada e Saída de dados com Scanner.

--- src/edu/joaquim/primeirasemana/CalculoBoletim.java
+++ src/edu/joaquim/primeirasemana/CalculoBoletim.java
@@ -1,17 +1,29 @@
 package edu.joaquim.primeirasemana;
 
+import java.util.Scanner;
+
 // Exercícios de identação + if else
 
 public class CalculoBoletim {
 	public static void main(String[] args) {
+
+		Scanner entradaDados = new Scanner(System.in);
+
+		System.out.print("Digite sua nota da primeira prova: ");
+		float notaProva1 = entradaDados.nextFloat();
+
+		System.out.print("Digite sua nota da segunda prova: ");
+		float notaProva2 = entradaDados.nextFloat();
+
+		float mediaFinal = (notaProva1 + notaProva2) / 2;
+
 		// Seguir uma identação correta são boas práticas.
-		int mediaFinal = 5;
 		if (mediaFinal < 6) {
-			System.out.println("Reprovado!");
+			System.out.format("Reprovado! Sua média final é: %.2f \n", mediaFinal);
 		} else if (mediaFinal == 6) {
-			System.out.println("Aprovado na média.");
+			System.out.format("Aprovado na média. Sua média final é: %.2f \n", mediaFinal);
 		} else {
-			System.out.println("Aprovado acima da média, parabéns!");
+			System.out.format("Aprovado acima da média, parabéns! Sua média final é: %.2f \n", mediaFinal);
 		}
 	}
 }
